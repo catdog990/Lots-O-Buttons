ad450831493cdb7b42f628b4b91e2cb02c882866
catdog990 <ozmcastro@gmail.com>
2017-12-18 09:13:36 -0500

    progress

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,45 @@
 $(document).ready(function(){
 
-var mahKey = REg9ubiXYsGOG7EZXI1QaFgDsVC3YkXq
+var mahKey = REg9ubiXYsGOG7EZXI1QaFgDsVC3YkXq;
 var Topics = ["Movies", "Sports", "Cars"];
 
 
+function TopInfo(){
+	var tops = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/" + tops + "?api_key=REg9ubiXYsGOG7EZXI1QaFgDsVC3YkXq&q=&limit=10&offset=0&rating=PG-13&lang=en";
+
+	$.ajax({
+		url: queryURL,
+		method:"GET"
+	}).done(function(response){
+		
+	})
 }
+
+
+
+
+
+function renderButtons(){
+	$("$HButtons").empty();
+	for (var i = 0; i < Topics.length; i++) {
+		var wut = $("<button>");
+		wut.addClass("gif");
+		wut.attr("data-name", Topics[i]);
+		wut.text(Topics[i])
+		$("$HButtons").append(wut);
+	}
+
+};
+
+
+
+// When Button is clicked//
+$("#searchB").on("click", function(event){
+	event.preventDefault();
+	var gif = $("#sumthang").val().trim();
+	Topics.push(gif);
+	renderButtons();
+});
+
+});
